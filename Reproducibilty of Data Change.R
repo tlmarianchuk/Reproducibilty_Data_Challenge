48743b13c2b8a94c16e65051cfc1ff8045c05bcd
Joash Lake <jlake1@uchicago.edu>
2019-09-11 16:08:32 -0400

    Importing the Data

--- Reproducibilty of Data Change.R
+++ Reproducibilty of Data Change.R
@@ -4,5 +4,6 @@ library(readr)
 library(ggplot2)
 library(dplyr)
 
-dat <- read.csv("mitchell_weevil_egg_data_1975.csv")
-View(dat)
+mdat <- read.csv("mitchell_weevil_egg_data_1975.csv")
+cdat <- read.csv("cole_arthropod_data_1946.csv")
+
